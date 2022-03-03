63151b8f7b0b428c704ccbaadb5c02e82e7b6b58
Mack Stanley <mlgprohacker@gmail.com>
2022-03-03 17:21:30 -0600

    make search endpoint

--- src/scripts/flaskapp.py
+++ src/scripts/flaskapp.py
@@ -5,7 +5,6 @@ from flask_cors import CORS
 import mysql.connector as sql
 from dotenv import load_dotenv
 from os import getenv
-import json
 
 load_dotenv()
 
@@ -22,17 +21,17 @@ def dbOpenConn():
     )
 
 
-@ app.route("/create", methods=["POST"])
+@app.route("/create", methods=["POST"])
 def create():
     pass
 
 
-@ app.route('/deploy', methods=['POST'])
+@app.route('/deploy', methods=['POST'])
 def deploy():
     pass
 
 
-@ app.route('/getall', methods=['GET'])
+@app.route('/getall', methods=['GET'])
 def getall():
     db = dbOpenConn()
     query = "SELECT * FROM items"
@@ -48,19 +47,32 @@ def getall():
     return jsonify(response)
 
 
-@ app.route('/remove', methods=['POST'])
+@app.route('/remove', methods=['POST'])
 def remove():
     pass
 
 
-@ app.route('/update', methods=['POST'])
+@app.route('/update', methods=['POST'])
 def update():
     pass
 
 
-@ app.route('/search', methods=['GET'])
+@app.route('/search', methods=['GET'])
 def search():
-    pass
+    searchterm = request.json["searchterm"]
+
+    db = dbOpenConn()
+    query = f"SELECT * FROM items WHERE name LIKE \"%{db.converter.escape(searchterm)}%\""
+    cursor = db.cursor(dictionary=True)
+
+    cursor.execute(query)
+
+    response = cursor.fetchall()
+
+    cursor.close()
+    db.close()
+
+    return jsonify(response)
 
 
 if __name__ == "__main__":
