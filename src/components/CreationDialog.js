a6bef85e28871600f79113e08c4ec830916a0d59
Mack Stanley <mlgprohacker@gmail.com>
2022-03-06 14:27:42 -0600

    wider text boxes

--- src/components/CreationDialog.js
+++ src/components/CreationDialog.js
@@ -7,9 +7,9 @@ export default function CreationDialog(props) {
   return (
     <div className="bg-white rounded-lg mb-5 sm:w-2/3 lg:w-1/3 self-center">
       <div className="p-3 space-y-3">
-        <div className="flex flex-col">
+        <div>
           <div>Name</div>
-          <div>
+          <div className="flex flex-col">
             <input
               type="text"
               name="name"
