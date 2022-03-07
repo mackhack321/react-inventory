b0f1f90da58378862901507c892006dc7bc400a6
Mack Stanley <mlgprohacker@gmail.com>
2022-03-07 16:31:51 -0600

    make pwa-friendly :o also decide against sort

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,7 @@
 /*
   TODO:
-  - sort
+  - better colors
+  - custom icon
 */
 
 import React, { useEffect, useState } from "react";
@@ -168,7 +169,7 @@ export default function Table() {
                       scope="col"
                       className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
-                      Actions
+                      {data.length} Items
                     </th>
                   </tr>
                 </thead>
